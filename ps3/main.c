8ff5e750c9d077641c73dd12ef5d968285b9cc5e
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2020-11-14 20:45:17 +0100

    Update main.c

---
+++ ps3/main.c
@@ -0,0 +1,15 @@
+#include "hangman.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main(){
+    srand(time(NULL));
+    char secret[30];
+    get_word(secret);
+    hangman(secret);
+    
+    return 0;
+}
