583e416e185e4de6665c037a55449a0dffbcdf42
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2020-11-25 17:06:19 +0100

    Update every_second.c

--- ps5/every_second.c
+++ ps5/every_second.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 char triger_start(int y);
+char triger_stop(int y);
+char go_up(char x);
 
 int main(int arg, char* subor[], char* subor2[]) {
 
@@ -8,6 +10,7 @@ int main(int arg, char* subor[], char* subor2[]) {
     FILE *fp2 = fopen(subor[2], "w");
     
     int n = 0;
+    int n2 = 0;
     int nn = 0;
     int kost1 = 0;
     //int len_cat = 0;
@@ -16,11 +19,10 @@ int main(int arg, char* subor[], char* subor2[]) {
     int parity = 0;
     
     char letter;
-    //char cat[30000];
-    
-    char res;
-    
     int ops = 0;
+    
+    char tie[10000];
+    int c = 0;
     
     while (1) {
         letter = fgetc(fp);
@@ -31,7 +33,7 @@ int main(int arg, char* subor[], char* subor2[]) {
         }
         
         // check START
-        if (kost1 == 0 && letter == triger_start(0)) {
+        if (kost1 == 0 && go_up(letter) == triger_start(0)) {
             n++;
             if (n==5) {
                 kost1 = 1;
@@ -48,20 +50,22 @@ int main(int arg, char* subor[], char* subor2[]) {
             if (nn != 0) {
                 
                 if(parity%2 == 0 && nn != 1){
-                    if (letter == 'S') {
-                        res = fgetc(fp);
-                        if (res == 'T') {
-                            res = fgetc(fp);
-                            if (res == 'O') {
-                                res = fgetc(fp);
-                                if (res == 'P') {
-                                    ops = 1;
-                                }
-                            }
+                    if (go_up(letter) == triger_stop(0)) {
+                        n2++;
+                        if (n2==4) {
+                            ops = 1;
+                            n2=0;
                         }
                     }
+                    else{
+                        triger_stop(1);
+                        n2=0;
+                    }
+                    
                     if (ops == 0) {
-                        fputc(letter, fp2);
+                        //fputc(letter, fp2);
+                        tie[c] = letter;
+                        c++;
                     }
                 }
                 if (letter == ' ') {
@@ -71,6 +75,21 @@ int main(int arg, char* subor[], char* subor2[]) {
             }
             nn++;
         }
+    }
+    if (go_up(tie[c-1]) == 'O') {
+        if (go_up(tie[c-2]) == 'T') {
+            if (go_up(tie[c-3]) == 'S') {
+                c -= 4;
+                //printf("dd\n");
+            }
+        }
+    }
+    else{
+        c-=1;
+    }
+    
+    for (int i = 0; i < c; i++) {
+        fputc(tie[i], fp2);
     }
     
     fclose(fp);
@@ -93,3 +112,24 @@ char triger_start(int y){
     return start[intn1];
 }
 
+char triger_stop(int y){
+    char start[] = "STOP";
+    static int intn1 = -1;
+    if (y == 1) {
+        intn1 = -1;
+        return 0;
+    }
+    intn1++;
+    if (intn1 == 4) {
+        intn1 = 0;
+    }
+    //printf("[%c]", start[intn1]);
+    return start[intn1];
+}
+
+char go_up(char x){
+    if( x <= 'z' && x >= 'a'){
+        x += 'A' - 'a';
+    }
+    return x;
+}
